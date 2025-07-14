564290b6c940fce9770ff76cec4a9197e075f749
Kyle Mistele <kyle@mistele.com>
2025-07-14 15:16:46 -0700

    remove w&b

--- packages/dashboard/src/lib/mcp/index.ts
+++ packages/dashboard/src/lib/mcp/index.ts
@@ -1,15 +1,8 @@
-import { init } from 'weave'
 import { registerDocumentationSearchTool } from './tools/documentation-search'
 import { registerSupportTool } from './tools/support'
 import type { McpServer, StaticMcpServerConfig } from './types'
 export { protectedResourceHandler } from './protected-resource-handler'
 export { withMcpAuth } from './with-mcp-auth'
-
-try {
-    init('kylemistele-naptha-ai/quickstart_playground')
-} catch (error) {
-    console.error(error)
-}
 
 /**
  * This function is called by the route handler to configure the MCP server object based on the static configuration from the database.
